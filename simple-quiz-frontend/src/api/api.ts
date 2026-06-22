48d492ad1072e5754001c659be2f9c0f42f415ea
dungnguyen2715 <bvmldung53@gmail.com>
2026-06-22 12:47:04 +0700

    production api

--- simple-quiz-frontend/src/api/api.ts
+++ simple-quiz-frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_BASE_URL } from "./constants";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL:  import.meta.env.VITE_API_URL || API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
